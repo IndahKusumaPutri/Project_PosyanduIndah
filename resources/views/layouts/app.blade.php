a301af183bd8c18b25863db172da34d729dd535b
Putri <106747173+IndahKusumaPutri@users.noreply.github.com>
2023-03-01 09:05:24 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,16 +64,17 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
         <!-- Control Sidebar -->
         {{-- <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-    <div class="p-3">
-      <h5>Title</h5>
-      <p>Sidebar content</p>
-    </div>
-  </aside> --}}
+        <!-- Control sidebar content goes here -->
+        <div class="p-3">
+            <h5>Title</h5>
+            <p>Sidebar content</p>
+        </div>
+        </aside> --}}
         <!-- /.control-sidebar -->
 
         <!-- Main Footer -->
         @include('layouts.footer')
+        
     </div>
     <!-- ./wrapper -->
 
